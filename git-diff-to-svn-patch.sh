35f759bba996107f58e6d6135cb02b61b870e71a
rambattu <sivarambattu@gmail.com>
2020-02-28 15:07:27 -0800

    Update git-diff-to-svn-patch.sh
    Comment about the source

--- git-diff-to-svn-patch.sh
+++ git-diff-to-svn-patch.sh
@@ -10,3 +10,8 @@ filename=`basename -s .diff $1`
 out_filename=${filename}-svn.patch
 echo "Writing SVN patch output to $out_filename"
 cat $1 | sed -e "s/^diff --git [^[:space:]]*/Index:/" -e "s/^index.*/===================================================================/" -e "s/^Index: b\//Index: /" -e "s/^--- a\//--- /" -e "s/^+++ b\//+++ /" > $out_filename    
+
+# Slight modification of the gist here https://gist.github.com/katta/1028871,
+# that script works with the diff that got generated from git using --no-prefix,
+# this script git-diff-to-svn-patch.sh can be used to work on diff that is generated
+# without "--no-prefix" option as well
